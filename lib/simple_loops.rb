e3d6a7e4f6356cde0ba93cfccbf8fa1a95a10115
outstanding-publisher-2341 <zaxmaf@gmail.com>
2020-08-10 00:46:05 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,8 +24,8 @@ def return_string_array(array)
   counter = 0
   new_array = []
   while counter < array.length do
-    array[counter] = array[counter].to_s
+    new_array << array[counter].to_s
     counter += 1
   end
-  array
+  new_array
 end
